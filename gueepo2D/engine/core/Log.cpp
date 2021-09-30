1d7e345ac7b4a96b577807a73318db4bc66d7aed
Guilherme de Oliveira <guilherme.061@gmail.com>
2021-09-30 17:31:42 -0400

    updating log

--- gueepo2D/engine/core/Log.cpp
+++ gueepo2D/engine/core/Log.cpp
@@ -5,7 +5,7 @@ namespace gueepo {
 	std::shared_ptr<spdlog::logger> Log::s_Logger;
 
 	void Log::Initialize() {
-		spdlog::set_pattern("%^[%T%L][%n]: %v%$ (%s:%#)");
+		spdlog::set_pattern("%^[%T%L][%n] %v%$ (%s:%#)");
 		s_Logger = spdlog::stdout_color_mt("gueepo2D");
 		s_Logger->set_level(spdlog::level::trace);
 		spdlog::set_default_logger(s_Logger);
