0212340ef7b3535d3361a330fa10b9c32555059c
Guilherme de Oliveira <guilherme.061@gmail.com>
2021-09-30 19:21:49 -0400

    forgot to commit main

--- gueepo2D/sandbox/Main.cpp
+++ gueepo2D/sandbox/Main.cpp
@@ -1,9 +1,51 @@
 #include <gueepo2d.h>
-#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
 
-int main(void) {
-	gueepo::Log::Initialize();
-	LOG_INFO("Logger Initialized!");
-	LOG_WARN("Logger Initialized!");
-	LOG_ERROR("Logger Initialized!");
+/*
+#include <SDL.h>
+#ifdef main
+#undef main
+#endif
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+*/
+
+class DummyApp : public gueepo::Application {
+public:
+	DummyApp() {}
+	~DummyApp() { LOG_INFO("deleting dummy app"); }
+};
+
+gueepo::Application* gueepo::CreateApplication() {
+	return new DummyApp();
 }
+
+/*
+int main(void) {
+	
+	SDL_Window* window = NULL;
+	SDL_Surface* screenSurface = NULL;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		LOG_ERROR("could not initialize sdl2: {0}", SDL_GetError());
+		return 1;
+	}
+	window = SDL_CreateWindow(
+		"hello_sdl2",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		SCREEN_WIDTH, SCREEN_HEIGHT,
+		SDL_WINDOW_SHOWN
+	);
+	if (window == NULL) {
+		LOG_ERROR("could not create window: {0}", SDL_GetError());
+		return 1;
+	}
+	screenSurface = SDL_GetWindowSurface(window);
+	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+	SDL_UpdateWindowSurface(window);
+	SDL_Delay(2000);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
+*/
