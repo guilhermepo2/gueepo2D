5d773952beed6256550d7dcc1a064f85d44b8007
Guilherme de Oliveira <guilherme.061@gmail.com>
2021-10-29 14:52:39 -0400

    [timestep] temporary approach on how to do it
    - the way to calculate delta time and the way to sleep should be platform specific

--- gueepo2D/engine/core/Application.cpp
+++ gueepo2D/engine/core/Application.cpp
@@ -3,6 +3,11 @@
 #include "core/events/ApplicationEvent.h"
 #include "core/events/EventDispatcher.h"
 
+// #todo: temp
+#include "SDL.h"
+const unsigned int FPS = 60;
+const unsigned int FRAME_TARGET_TIME = 1000 / FPS;
+
 namespace gueepo {
 
 #define BIND_EVENT(x) std::bind(&x, this, std::placeholders::_1)
@@ -18,16 +23,30 @@ namespace gueepo {
 
 	void Application::Run() {
 		m_bIsRunning = true;
+		float TicksLastFrame = 0.0f;
 
 		LOG_INFO("application is running!");
 		// #todo: call Application::Start here?
 		while (m_bIsRunning) {
-			// #todo: calculate delta time
-			for (Layer* l : m_LayerStack) {
-				l->OnUpdate(.1f);
-			}
 
+			// #todo: this has to be in its own platform independent class
+			float DeltaTime = static_cast<float>((SDL_GetTicks() - TicksLastFrame)) / 1000.0f;
+			DeltaTime = (DeltaTime > 0.05f) ? 0.05f : DeltaTime;
+			TicksLastFrame = SDL_GetTicks();
+			// ----------------------------------------------------------
+
+			for (Layer* l : m_LayerStack) {
+				l->OnUpdate(DeltaTime);
+			}
 			m_Window->Update();
+
+			// ----------------------------------------------------------
+			// delaying until next frame so we can keep 60fps
+			int TimeToWait = FRAME_TARGET_TIME - (SDL_GetTicks() - TicksLastFrame);
+			if (TimeToWait > 0.0f && TimeToWait <= FRAME_TARGET_TIME) {
+				SDL_Delay(TimeToWait);
+			}
+			// ----------------------------------------------------------
 		}
 	}
 
