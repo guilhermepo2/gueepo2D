8e30174541e80ac8a3d919ddec401f5d12a3e136
Guilherme de Oliveira <guilherme.061@gmail.com>
2021-11-12 13:36:56 -0500

    [renderer] adding a mess to application that I will to clean up later lmao

--- gueepo2D/engine/core/Application.cpp
+++ gueepo2D/engine/core/Application.cpp
@@ -6,6 +6,9 @@
 #include "core/input/Input.h"
 #include "core/TimeStep.h"
 
+#include <SDL.h>
+#include <glad/glad.h>
+
 // For now locking this to run at 60fps
 const unsigned int FPS = 60;
 const unsigned int FRAME_TARGET_TIME = 1000 / FPS;
@@ -38,8 +41,87 @@ namespace gueepo {
 		InputSystem* inputSystem = InputSystem::s_Instance;
 		inputSystem->Initialize();
 
+		const char* vertexShaderSource = "#version 330 core\n"
+			"layout (location = 0) in vec3 aPos;\n"
+			"void main()\n"
+			"{\n"
+			"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+			"}\0";
+		const char* fragmentShaderSource = "#version 330 core\n"
+			"out vec4 FragColor;\n"
+			"void main()\n"
+			"{\n"
+			"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+			"}\n\0";
+
+		// build and compile our shader program
+		// ------------------------------------
+		// vertex shader
+		unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+		glCompileShader(vertexShader);
+		// check for shader compile errors
+		int success;
+		char infoLog[512];
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+			LOG_WARN("ERROR::SHADER::VERTEX::COMPILATION_FAILED: {0}\n", infoLog);
+		}
+		// fragment shader
+		unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+		glCompileShader(fragmentShader);
+		// check for shader compile errors
+		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+			LOG_WARN("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED: {0}\n", infoLog);
+		}
+		// link shaders
+		unsigned int shaderProgram = glCreateProgram();
+		glAttachShader(shaderProgram, vertexShader);
+		glAttachShader(shaderProgram, fragmentShader);
+		glLinkProgram(shaderProgram);
+		// check for linking errors
+		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+		if (!success) {
+			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+			LOG_WARN("ERROR::SHADER::PROGRAM::LINKING_FAILED: {0}\n", infoLog);
+		}
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+
+		// set up vertex data (and buffer(s)) and configure vertex attributes
+		// ------------------------------------------------------------------
+		float vertices[] = {
+			-0.5f, -0.5f, 0.0f, // left  
+			 0.5f, -0.5f, 0.0f, // right 
+			 0.0f,  0.5f, 0.0f  // top   
+		};
+
+		unsigned int VBO, VAO;
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+		glBindVertexArray(VAO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+		// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+		glBindVertexArray(0);
+
 		LOG_INFO("application is running!");
-		// #todo: call Application::Start here?
 		while (m_bIsRunning) {
 			float DeltaTime = static_cast<float>((timestep::GetTicks() - TicksLastFrame)) / 1000.0f;
 			TicksLastFrame = timestep::GetTicks();
@@ -60,7 +142,11 @@ namespace gueepo {
 				l->OnImGuiRender();
 			}
 			m_ImGuiLayer->End();
-			
+
+			glUseProgram(shaderProgram);
+			glBindVertexArray(VAO);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+
 			m_Window->Update();
 			// delaying until next frame so we can keep 60fps
 			int TimeToWait = FRAME_TARGET_TIME - (timestep::GetTicks() - TicksLastFrame);
