973b746171fd16e8dd59acacb63b9f620b85c0b9
Guilherme Oliveira <guilherme.061@gmail.com>
2021-12-22 15:11:36 -0300

    qol: removing warnings from spdlog

--- gueepo2D/engine/core/Log.cpp
+++ gueepo2D/engine/core/Log.cpp
@@ -1,6 +1,9 @@
 #include "gueepo2Dpch.h"
 #include "Log.h"
+
+#pragma warning(push, 0)
 #include <spdlog/sinks/stdout_color_sinks.h>
+#pragma warning(pop)
 
 namespace gueepo {
 	std::shared_ptr<spdlog::logger> Log::s_Logger;
