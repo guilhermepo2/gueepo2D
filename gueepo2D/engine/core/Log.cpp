67b2c54276ce92ff008457e28f8d84133c301ae3
Guilherme de Oliveira <guilherme.061@gmail.com>
2022-04-08 08:48:29 -0400

    logging to files
    - something to be careful here is that every time the application starts the log file will be overwritten
    - how to address that? count how many logs files before creating a new one? renaming the log file when closing the application? making a copy of the log file when closing the application? can I not address that?

--- gueepo2D/engine/core/Log.cpp
+++ gueepo2D/engine/core/Log.cpp
@@ -3,15 +3,32 @@
 
 #pragma warning(push, 0)
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <filesystem>
 #pragma warning(pop)
 
 namespace gueepo {
 	std::shared_ptr<spdlog::logger> Log::s_Logger;
 
 	void Log::Initialize() {
-		spdlog::set_pattern("%^[%T%L][%n] %v%$ (%s:%#)");
-		s_Logger = spdlog::stdout_color_mt("gueepo2D");
+
+		// #todo: route this to gueepo::filesystem
+		std::string logsDir = "logs";
+		if (!std::filesystem::exists(logsDir)) {
+			std::filesystem::create_directories(logsDir);
+		}
+
+		std::vector<spdlog::sink_ptr> loggerSinks = {
+			std::make_shared<spdlog::sinks::stdout_color_sink_mt>(),
+			std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/gueepo2D.log", true)
+		};
+
+		loggerSinks[0]->set_pattern("%^[%T%L][%n] %v%$ (%s:%#)");
+		loggerSinks[1]->set_pattern("%^[%T%L][%n] %v%$ (%s:%#)");
+
+		s_Logger = std::make_shared<spdlog::logger>("gueepo2D", loggerSinks.begin(), loggerSinks.end());
 		s_Logger->set_level(spdlog::level::trace);
+
 		spdlog::set_default_logger(s_Logger);
 	}
 }
