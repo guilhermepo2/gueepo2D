19c1efe6948f16bca46685a6990ff1900f6542b7
Guilherme de Oliveira <guilherme.061@gmail.com>
2022-05-14 15:55:19 -0400

    creating the color buffer

--- gueepo2D/sandbox/PixelSandbox.cpp
+++ gueepo2D/sandbox/PixelSandbox.cpp
@@ -1,5 +1,6 @@
 #ifdef PIXEL_SANDBOX
 #include "PixelSandbox.h"
+#include <imgui.h>
 
 static const int WIDTH = 800;
 static const int HEIGHT = 600;
@@ -10,12 +11,14 @@ static gueepo::PixelBuffer* pixelBuffer = nullptr;
 #define EMPTY 0
 #define SAND_ID 1
 #define WATER_ID 2
+#define SMOKE_ID 3
+uint32_t currentlySelectedParticle = 0;
 
 #define PositionToIndex(x, y) ((WIDTH * y) + x)
 
 typedef struct {
 	uint8_t id = 0;
-	// float lifetime;
+	float lifetime;
 	// gueepo::math::vec2 velocity;
 	uint32_t color = 0;
 } cell_t;
@@ -23,7 +26,7 @@ typedef struct {
 static cell_t* s_all_cells;
 
 bool is_empty_cell(uint32_t x, uint32_t y) {
-	if (x < 0 || y < 0 || x > WIDTH || y > HEIGHT) {
+	if (x < 0 || y < 0 || x >= WIDTH  || y >= HEIGHT) {
 		return false;
 	}
 
@@ -48,6 +51,16 @@ void create_water_particle(uint32_t x, uint32_t y) {
 	s_all_cells[PositionToIndex(x, y)].color = 0xFFFF0000;
 }
 
+void create_smoke_particle(uint32_t x, uint32_t y) {
+	if (x < 0 || y < 0 || x > WIDTH || y > HEIGHT) {
+		return;
+	}
+
+	s_all_cells[PositionToIndex(x, y)].id = SMOKE_ID;
+	s_all_cells[PositionToIndex(x, y)].color = 0xFF333333;
+	s_all_cells[PositionToIndex(x, y)].lifetime = 2.0f;
+}
+
 void update_sand(uint32_t x, uint32_t y) {
 	bool didWeMove = false;
 
@@ -64,6 +77,35 @@ void update_sand(uint32_t x, uint32_t y) {
 	}
 	else if (is_empty_cell(x + 1, y - 1)) {
 		create_sand_particle(x + 1, y - 1);
+		didWeMove = true;
+	}
+
+	if (didWeMove) {
+		s_all_cells[PositionToIndex(x, y)].id = EMPTY;
+	}
+}
+
+void update_smoke(uint32_t x, uint32_t y, float DeltaTime) {
+	if (s_all_cells[PositionToIndex(x, y)].lifetime < 0.0f) {
+		s_all_cells[PositionToIndex(x, y)].id = EMPTY;
+		return;
+	}
+
+	
+	s_all_cells[PositionToIndex(x, y)].lifetime -= DeltaTime;
+
+	bool didWeMove = false;
+
+	if (is_empty_cell(x, y + 1)) {
+		create_smoke_particle(x, y + 1);
+		didWeMove = true;
+	}
+	else if (is_empty_cell(x - 1, y + 1)) {
+		create_smoke_particle(x - 1, y + 1);
+		didWeMove = true;
+	}
+	else if (is_empty_cell(x + 1, y + 1)) {
+		create_smoke_particle(x + 1, y + 1);
 		didWeMove = true;
 	}
 
@@ -118,7 +160,7 @@ public:
 	void OnInput(const gueepo::InputState& currentInputState) override;
 	void OnEvent(gueepo::Event& e) override {}
 	void OnRender() override;
-	void OnImGuiRender() override {}
+	void OnImGuiRender() override;
 
 private:
 	std::unique_ptr<gueepo::OrtographicCamera> m_Camera;
@@ -139,7 +181,6 @@ void PixelLayer::OnAttach() {
 
 void PixelLayer::OnDetach() {
 	delete pixelBuffer;
-	// delete[] tex_data;
 	delete[] s_all_cells;
 }
 
@@ -154,6 +195,9 @@ void PixelLayer::OnUpdate(float DeltaTime) {
 			case WATER_ID:
 				update_water(x, y);
 				break;
+			case SMOKE_ID:
+				update_smoke(x, y, DeltaTime);
+				break;
 			default:
 				break;
 			}
@@ -162,17 +206,6 @@ void PixelLayer::OnUpdate(float DeltaTime) {
 }
 
 void PixelLayer::OnInput(const gueepo::InputState& currentInputState) {
-	if (currentInputState.Mouse.IsMouseKeyDown(gueepo::MOUSE_LEFT)) {
-		gueepo::math::vec2 pos = currentInputState.Mouse.GetPosition();
-		int pos_x = (int)pos.x;
-		int pos_y = HEIGHT - (int)pos.y;
-
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				create_water_particle(pos_x + i, pos_y + j);
-			}
-		}
-	}
 
 	if (currentInputState.Mouse.IsMouseKeyDown(gueepo::MOUSE_RIGHT)) {
 		gueepo::math::vec2 pos = currentInputState.Mouse.GetPosition();
@@ -181,7 +214,17 @@ void PixelLayer::OnInput(const gueepo::InputState& currentInputState) {
 
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
-				create_sand_particle(pos_x + i, pos_y + j);
+				switch (currentlySelectedParticle) {
+				case SAND_ID:
+					create_sand_particle(pos_x + i, pos_y + j);
+					break;
+				case WATER_ID:
+					create_water_particle(pos_x + i, pos_y + j);
+					break;
+				case SMOKE_ID:
+					create_smoke_particle(pos_x + i, pos_y + j);
+					break;
+				}
 			}
 		}
 	}
@@ -210,6 +253,23 @@ void PixelLayer::OnRender() {
 	pixelBuffer->Render();
 	gueepo::Renderer::EndScene();
 }
+
+void PixelLayer::OnImGuiRender() {
+	ImGui::Begin("Particle Selector");
+	if (ImGui::Button("sand")) {
+		currentlySelectedParticle = 1;
+	}
+	if (ImGui::Button("water")) {
+		currentlySelectedParticle = 2;
+	}
+	if (ImGui::Button("smoke")) {
+		currentlySelectedParticle = 3;
+	}
+	ImGui::End();
+}
+
+// =======================================================================
+// =======================================================================
 
 PixelApp::PixelApp() : Application("pixel app", WIDTH, HEIGHT) {
 	PushLayer(new PixelLayer());
