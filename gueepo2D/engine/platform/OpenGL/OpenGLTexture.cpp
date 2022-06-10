ae12c3e78692966c5365eb751afd16ef37005caa
Guilherme de Oliveira <guilherme.061@gmail.com>
2022-06-10 18:08:34 -0400

    adding shader for fonts

--- gueepo2D/engine/platform/OpenGL/OpenGLTexture.cpp
+++ gueepo2D/engine/platform/OpenGL/OpenGLTexture.cpp
@@ -46,6 +46,8 @@ namespace gueepo {
 
 		glBindTexture(GL_TEXTURE_2D, m_textureID);
 		glTexImage2D(GL_TEXTURE_2D, 0, m_format, m_width, m_height, 0, m_format, GL_UNSIGNED_BYTE, nullptr);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		glTextureParameteri(m_textureID, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTextureParameteri(m_textureID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	}
