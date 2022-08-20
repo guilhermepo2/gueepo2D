3dd05c4d07d226bc60701585cd965773e9047c19
Guilherme Oliveira <guilherme.061@gmail.com>
2022-08-19 17:43:18 -0700

    Update OpenGLTexture.cpp

--- gueepo2D/engine/platform/OpenGL/OpenGLTexture.cpp
+++ gueepo2D/engine/platform/OpenGL/OpenGLTexture.cpp
@@ -29,12 +29,12 @@ namespace gueepo {
 		m_isLoaded = true;
 
 		glBindTexture(GL_TEXTURE_2D, m_textureID);
-		
-		glTexParameteri(m_textureID, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(m_textureID, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
 		glTexParameteri(m_textureID, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(m_textureID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		glTexParameteri(m_textureID, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(m_textureID, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
 		glTexImage2D(GL_TEXTURE_2D, 0, m_format, m_width, m_height, 0, m_format, GL_UNSIGNED_BYTE, textureData.texture_data);
 	}
@@ -75,7 +75,7 @@ namespace gueepo {
 
 	void OpenGLTexture::Bind(uint32_t slot /*= 0*/) const {
 		// glBindTextureUnit(slot, m_textureID);
-		
+
 		glActiveTexture(GL_TEXTURE0 + slot);
 		glBindTexture(GL_TEXTURE_2D, m_textureID);
 	}
