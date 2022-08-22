23d900ee9c73a61bfad36f77d4b3eccc8d5230a2
Guilherme Oliveira <guilherme.061@gmail.com>
2022-08-21 23:36:28 -0700

    turns out the tex parameters were all wrong

--- gueepo2D/engine/platform/OpenGL/OpenGLTexture.cpp
+++ gueepo2D/engine/platform/OpenGL/OpenGLTexture.cpp
@@ -30,11 +30,11 @@ namespace gueepo {
 
 		glBindTexture(GL_TEXTURE_2D, m_textureID);
 
-		glTexParameteri(m_textureID, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(m_textureID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-		glTexParameteri(m_textureID, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(m_textureID, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
 		glTexImage2D(GL_TEXTURE_2D, 0, m_format, m_width, m_height, 0, m_format, GL_UNSIGNED_BYTE, textureData.texture_data);
 	}
