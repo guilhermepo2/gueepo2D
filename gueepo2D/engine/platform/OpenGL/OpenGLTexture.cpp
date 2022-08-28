dfa2ad3a927a12cdf69147dcaff82cbdc887ebb4
Guilherme Oliveira <guilherme.061@gmail.com>
2022-08-28 11:20:20 -0700

    saul wrong (fixing OpenGLTexture)
    - using glTexParameteri(3.3+) instead of using glTextureParameteri(4.5+) so mac os doesn't get angry at us
    - using GL_TEXTURE_2D on that same function instead of the texture ID because that's just how you are supposed to use it and it was wrong before

--- gueepo2D/engine/platform/OpenGL/OpenGLTexture.cpp
+++ gueepo2D/engine/platform/OpenGL/OpenGLTexture.cpp
@@ -52,8 +52,8 @@ namespace gueepo {
 		glTexImage2D(GL_TEXTURE_2D, 0, m_format, m_width, m_height, 0, m_format, GL_UNSIGNED_BYTE, nullptr);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTextureParameteri(m_textureID, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTextureParameteri(m_textureID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	}
 
 	OpenGLTexture::~OpenGLTexture() {
