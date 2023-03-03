4099dbad114441972bffdec6d85290b0a309408a
Guilherme Oliveira <guilherme.061@gmail.com>
2023-03-02 18:02:36 -0800

    addressing internal format on fonts so it works on webgl

--- gueepo2D/engine/platform/OpenGL/OpenGLTexture.cpp
+++ gueepo2D/engine/platform/OpenGL/OpenGLTexture.cpp
@@ -44,16 +44,21 @@ namespace gueepo {
 		m_textureID = 0;
 		m_width = width;
 		m_height = height;
+
+        int internalFormat = bIsFont ? GL_R8 : GL_RGBA;
 		m_format = bIsFont ? GL_RED : GL_RGBA;
 
 		glGenTextures(1, &m_textureID);
 
 		glBindTexture(GL_TEXTURE_2D, m_textureID);
-		glTexImage2D(GL_TEXTURE_2D, 0, m_format, m_width, m_height, 0, m_format, GL_UNSIGNED_BYTE, nullptr);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, m_width, m_height, 0, m_format, GL_UNSIGNED_BYTE, nullptr);
 	}
 
 	OpenGLTexture::~OpenGLTexture() {
